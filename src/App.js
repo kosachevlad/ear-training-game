57e7c4e70aa5429988e4948a40fe975927953a48
kosachevlad@gmail.com <kosachevlad@gmail.com>
2025-03-30 20:02:21 +0300

    eslint fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import * as Tone from "tone";
 import { Renderer, Stave, StaveNote, Voice, Formatter, Accidental } from "vexflow";
